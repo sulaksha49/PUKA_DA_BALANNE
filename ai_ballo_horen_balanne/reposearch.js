0c8a2aea680ff83d746738f944ebd344bf1f8c22
sulaksha49 <sulakshamadara49@gmail.com>
2025-03-09 09:01:59 +0530

    Update reposearch.js

--- ai_ballo_horen_balanne/reposearch.js
+++ ai_ballo_horen_balanne/reposearch.js
@@ -25,7 +25,7 @@ cmd({
     responseMsg += `🍴 *Forks*: ${data.forks_count}\n`;
     responseMsg += `👤 *Owner*: ${data.owner.login}\n`;
     responseMsg += `📅 *Created At*: ${new Date(data.created_at).toLocaleDateString()}\n`;
-    responseMsg += `\n> *© Powered by JawadTechX*`;
+    responseMsg += `\n> 🄿🄾🅆🄴🅁🄳 🅱🆈 𝐒𝐔𝐋𝐀_𝐌𝐃 😈`;
 
     await conn.sendMessage(from, { text: responseMsg }, { quoted: m });
   } catch (error) {
