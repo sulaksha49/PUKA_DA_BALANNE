ce92435e50c35d7e85adb970ae45baac461083d0
sulaksha49 <sulakshamadara49@gmail.com>
2025-03-14 05:20:54 +0530

    Update mp3.js

--- ai_ballo_horen_balanne/mp3.js
+++ ai_ballo_horen_balanne/mp3.js
@@ -1,3 +1,7 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); 
+
 cmd({
     pattern: "song",
     alias: ["ytmp3", "yta"],
