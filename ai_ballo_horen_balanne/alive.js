26d970890f261d4f06dca93d74579f4457f6fba7
sulaksha49 <sulakshamadara49@gmail.com>
2025-03-16 09:00:38 +0530

    Update alive.js

--- ai_ballo_horen_balanne/alive.js
+++ ai_ballo_horen_balanne/alive.js
@@ -30,7 +30,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send the status message with an image
         await conn.sendMessage(from, { 
-            image: { url: `https://files.catbox.moe/5v4ewp.jpg` },  // Image URL
+            image: { url: `https://i.ibb.co/Y7X4ScqS/SulaMd.png` },  // Image URL
             caption: status,
             contextInfo: {
                 mentionedJid: [m.sender],
