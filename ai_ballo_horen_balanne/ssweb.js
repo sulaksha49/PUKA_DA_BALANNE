7014e393dfcb9491abaec0f0bd8b6108efb9fcf0
sulaksha49 <sulakshamadara49@gmail.com>
2025-03-17 06:55:51 +0530

    Create ssweb.js

---
+++ ai_ballo_horen_balanne/ssweb.js
@@ -0,0 +1,53 @@
+
+
+
+const axios = require("axios");
+const config = require('../config');
+const { cmd } = require('../command');
+
+cmd({
+  pattern: "ss",
+  alias: ["ssweb"],
+  react: "🚀",
+  desc: "Download screenshot of a given link.",
+  category: "other",
+  use: ".ss <link>",
+  filename: __filename,
+}, 
+async (conn, mek, m, {
+  from, l, quoted, body, isCmd, command, args, q, isGroup, sender, 
+  senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, 
+  groupMetadata, groupName, participants, isItzcp, groupAdmins, 
+  isBotAdmins, isAdmins, reply 
+}) => {
+  if (!q) {
+    return reply("Please provide a URL to capture a screenshot.");
+  }
+
+  try {
+    
+    const response = await axios.get(`https://bk9.fun/tools/screenshot?device=tablet&url= ${q}`);
+    const screenshotUrl = response.data.screenshotUrl;
+
+    // give credit and use
+    const imageMessage = {
+      image: { url: screenshotUrl },
+      caption: "*𝐒𝐔𝐋𝐀 SS WEB*\n\n> 🄿🄾🅆🄴🅁🄳 🅱🆈 𝐒𝐔𝐋𝐀_𝐌𝐃 😈",
+      contextInfo: {
+        mentionedJid: [m.sender],
+        forwardingScore: 999,
+        isForwarded: true,
+        forwardedNewsletterMessageInfo: {
+          newsletterJid: '120363385281017920@newsletter',
+          newsletterName: "𝐒𝐔𝐋𝐀-𝐌𝐃",
+          serverMessageId: 1,
+        },
+      },
+    };
+
+    await conn.sendMessage(from, imageMessage, { quoted: m });
+  } catch (error) {
+    console.error(error);
+    reply("Failed to capture the screenshot. Please try again.");
+  }
+});
